7123bde4d78851f387899608b605552f409f8316
vmee <konsj@sina.cn>
2015-02-26 18:30:36 +0800

    business

--- lang/zh-cn/sms.inc.php
+++ lang/zh-cn/sms.inc.php
@@ -7,7 +7,7 @@ defined('IN_DESTOON') or exit('Access Denied');
 //$L['sms_code'] = '您的短信认证码为:{V0},有效期{V1}天';
 $L['sms_code'] = '您的验证码是{V0}。如非本人操作，请忽略本短信';
 $L['sms_business'] = '商家{V0}申请入驻,电话{V1}';
-$L['sms_invite'] = '您的注册邀请码是{V0}。欢迎注册,查看更多优惠信息';
+$L['sms_invite'] = '您的注册邀请码是{V0}。欢迎注册www.hezejiehun.com,查看更多优惠信息';
 $L['sms_inquiry'] = '您的产品[{V0}](ID:{V1}),收到询价.联系人:{V2},电话:{V3}';
 $L['sms_price'] = '您的求购[{V0}](ID:{V1}),收到报价.联系人:{V2},电话:{V3}';
 $L['sms_message'] = '您发布的[{V0}](ID:{V1}),收到留言.联系人:{V2},电话:{V3}';
