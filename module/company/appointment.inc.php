ec6b2e95ec64a0007afadbc8b85d547e442c9a1d
vmee <konsj@sina.cn>
2015-05-25 07:50:27 +0800

    appointment

---
+++ module/company/appointment.inc.php
@@ -0,0 +1,44 @@
+<?php
+defined('IN_DESTOON') or exit('Access Denied');
+
+if($_userid){
+
+    if($submit) {
+
+
+        //$session = new dsession();
+        //if($_SESSION['mobile_code'] != md5($_SESSION['mobile'].'|'.mobilecode)) exit(json_encode(array('info'=>'手机验证码错误', 'status'=>'n')));
+
+        $post = dhtmlspecialchars($post);
+        $post = array_map("trim", $post);
+
+        $post['invite_username'] = $username;
+
+        if($_userid){
+            $user = userinfo($_username);
+            $post['username'] = $_username;
+            $post['areaid'] = $user['areaid'];
+            $post['truename'] = $user['truename'];
+        }
+
+        $post['addtime'] = time();
+
+        $sqlk = $sqlv = '';
+        foreach($post as $k=>$v) {
+            $sqlk .= ','.$k; $sqlv .= ",'$v'";
+        }
+        $sqlk = substr($sqlk, 1);
+        $sqlv = substr($sqlv, 1);
+
+
+        $itemid = $db->query("INSERT INTO {$DT_PRE}appointment ($sqlk) VALUES ($sqlv)");
+
+
+        exit(json_encode(array('info'=>'预约成功', 'status'=>'y')));
+    }else{
+        $mid = $mid ? $mid : 2;
+        $title = $title ? $title : $COM['company'];
+    }
+}
+
+include template('appointment', $template);
